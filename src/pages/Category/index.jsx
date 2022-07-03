70618d3263f789dd96fb9ede2004344f4e5b2673
test <test>
2022-07-03 22:06:24 +0500

    finished data fetchin on page categories

--- src/pages/Category/index.jsx
+++ src/pages/Category/index.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
 import Header from '../../components/Header'
+import ProductCard from '../../components/ProductCard'
+import Container from '../../layout/Container'
+import classes from './Category.module.scss'
 
 const Category = () => {
+  const { type } = useParams()
+  const [data, setData] = useState(null)
+  const fetchData = () => {
+    fetch('http://localhost:4000/products').then((data) => {
+      return data.json()
+    }).then((cards) => {
+      setData(cards)
+    })
+    console.log(data);
+  }
   return (
-    <Header/>
+    <>
+      <Header />
+      <Container className={classes["cards"]}>
+        {data && data.map(card =>
+          <ProductCard key={card.id} data={card} className={classes["card-item"]} />
+          )}
+        <button onClick={fetchData}>fetchData</button>
+      </Container>
+    </>
   )
 }
 
